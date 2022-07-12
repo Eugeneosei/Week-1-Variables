4b94f30f47553f4a75ab6a0116df57c334de8f94
Eugeneosei <eugeneosei37@gmail.com>
2022-07-12 18:31:12 -0400

    first commit

---
+++ VariablesAndOperations.java
@@ -0,0 +1,27 @@
+package VariablesAndOperations;
+
+public class VariablesAndOperations {
+
+	public static void main(String[] args) {
+		
+	int avaiablePlaneSeats = 5;
+	
+	double costOfGroceries =23.64;
+	
+	char middleInitial = 'C';
+	
+	boolean isHotOutside = false;
+	
+	String customerFirstName = "Zion";
+	
+	String streetAddress = "1234 w Easy St";
+	
+	
+	System.out.print(avaiablePlaneSeats);
+	System.out.print(costOfGroceries);
+	System.out.print(middleInitial);
+	System.out.print(isHotOutside);
+	System.out.print(customerFirstName);
+	System.out.print(streetAddress);
+}
+}
